1cee4f1f3307f3f4af0352bedbc50d12e9109995
Ricardo Lino <ricardo.mozart@gmail.com>
2014-12-09 07:36:38 -0200

    alterando classe person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
+require "digest/sha1"
+require 'single_test/tasks'
+
 class Person < ActiveRecord::Base
 
 	validates :name, presence: true, length: {maximum: 50}
@@ -5,8 +8,27 @@ class Person < ActiveRecord::Base
 	validates :born_at, presence: true
 	validate :age_limit
 
+	attr_accessor :plain_password
+  	before_save   :encrypt_password
+
+  	scope :admins, -> { where(["admin=?", true]) }
+  	scope :normal, -> { where(["admin=?", false]) }
+
+  	def self.encrypt_password(pwd)
+    	Digest::SHA1.hexdigest("123_#{pwd}_456")
+	end
+
+	def self.auth(email, password)
+		where(["email=? and password=?", email, encrypt_password(password)]).first
+	end 
+
 	private 
 		def age_limit
 			errors.add(:born_at,"tem que ser maior que 16 anos") if self.born_at.nil? || Date.today.year-self.born_at.year<16
+		end
+
+		def encrypt_password
+			return if plain_password.to_s.strip.size < 1 	
+			self.password = Person.encrypt_password(plain_password)
 		end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -45,4 +45,53 @@ class PersonTest < ActiveSupport::TestCase
 		@person.born_at = Date.today - 17.years
 		assert @person.valid?
 	end
+
+	test "tem que retornar a senha encriptada correta" do
+		assert_equal Digest::SHA1.hexdigest("123_teste_456"), Person.encrypt_password("teste")
+	end
+
+	test "tem que ter atributo para receber a senha em texto puro" do assert_respond_to @person, :plain_password=
+	end
+
+	test "tem que ter atributo para retornar a senha em texto puro como vazia" do assert_respond_to @person, :plain_password
+		assert_nil @person.plain_password
+	end
+
+	test "tem que gravar a senha encriptada no atributo password" do @person.plain_password = "teste"
+		@person.save
+		assert_equal Person.encrypt_password("teste"), @person.password
+	end
+
+	test "nao deve alterar a senha encriptada se enviada senha em texto puro vazia"do
+		@person.plain_password = "teste"
+		old_encrypted = @person.password
+		@person.plain_password = ""
+		@person.save
+		assert_equal old_encrypted, @person.password
+	end
+
+	test "nao deve alterar a senha encriptada se enviada senha em texto puro nula" do
+		@person.plain_password = "teste"
+		old_encrypted = @person.password
+		@person.plain_password = nil
+		@person.save
+		assert_equal old_encrypted, @person.password
+	end
+
+	test "deve ter metodo para autenticacao" do 
+		assert_respond_to Person, :auth
+	end
+
+	test "nao deve retornar objeto se email errado" do 
+		assert_nil Person.auth("foo@bar", "teste")
+	end
+
+	test "deve retornar objeto se email e senha corretos" do
+		person = Person.auth(@person.email,"secret")
+		assert_not_nil person
+		assert_kind_of Person, person
+		assert_equal @person.name, person.name
+		assert_equal @person.email, person.email
+	end
+
 end
